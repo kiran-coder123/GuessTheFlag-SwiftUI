8d6563ab7bdb4318f91a26da7a4f265962cc570a
kiran sonne <kiransonne04@gmail.com>
2022-11-17 14:11:40 +0530

    Initial Commit

---
+++ GuessTheFlagApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlagApp
+//
+//  Created by Kiran Sonne on 17/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlagApp/GuessTheFlagAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagAppApp.swift
+//  GuessTheFlagApp
+//
+//  Created by Kiran Sonne on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
